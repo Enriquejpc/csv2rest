52edf9d72bd40e3021dd2e12bff7a173987938dd
James Smith <james@floppy.org.uk>
2016-06-17 11:47:50 +0100

    rename tests

--- spec/csv2rest_spec.rb
+++ spec/csv2rest_spec.rb
@@ -5,27 +5,32 @@ describe Csv2rest do
     expect(Csv2rest::VERSION).not_to be nil
   end
 
-  it 'turns a simple CSV into simple JSON' do
+  context 'given a CSV with schema it' do
     
-    @csv = "file://" + File.join(File.dirname(__FILE__), "fixtures", "tomato-types.csv")
-    @schema = Csvlint::Schema.load_from_json(File.join(File.dirname(__FILE__), "fixtures", "tomato-types.csv-metadata.json"))
+    before :all do
+      @csv = "file://" + File.join(File.dirname(__FILE__), "fixtures", "tomato-types.csv")
+      @schema = Csvlint::Schema.load_from_json(File.join(File.dirname(__FILE__), "fixtures", "tomato-types.csv-metadata.json"))
+    end
     
-    g = Csv2rest.generate @csv, @schema
-    
-    expect(g['/tomato-types/cordon']).to eq (
-      {
-        "type" => "cordon",
-        "also called" => "indeterminate",
-        "description" => "grows very tall"
-      }
-    )
+    it "generates individual JSON files" do
+      g = Csv2rest.generate @csv, @schema
+      
+      expect(g['/tomato-types/cordon']).to eq (
+        {
+          "type" => "cordon",
+          "also called" => "indeterminate",
+          "description" => "grows very tall"
+        }
+      )
 
-    expect(g['/tomato-types/bush']).to eq (
-      {
-        "type" => "bush",
-        "also called" => "determinate",
-        "description" => "does not require pruning"
-      }
-    )
+      expect(g['/tomato-types/bush']).to eq (
+        {
+          "type" => "bush",
+          "also called" => "determinate",
+          "description" => "does not require pruning"
+        }
+      )
+    end
+    
   end
 end
