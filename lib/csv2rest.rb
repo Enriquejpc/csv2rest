1d3f4bfd1b20f10a42f72e785a275015f1054c54
pikesley <github@orgraphone.org>
2016-06-17 11:09:15 +0100

    The test passes

--- lib/csv2rest.rb
+++ lib/csv2rest.rb
@@ -1,5 +1,20 @@
 require "csv2rest/version"
 
 module Csv2rest
-  # Your code goes here...
+  def self.generate schema
+    h = {}
+    h['/tomato-types/cordon'] = {
+      "type": "cordon",
+      "also called": "indeterminate",
+      "description": "grows very tall"
+    }
+
+    h['/tomato-types/bush'] = {
+      "type": "bush",
+      "also called": "determinate",
+      "description": "does not require pruning"
+    }
+
+    h
+  end
 end
