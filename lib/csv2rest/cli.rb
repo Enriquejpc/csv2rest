a2655e9fedf921e6f9a61d982c9c1d0b7a5bcc3e
James Smith <james@floppy.org.uk>
2016-06-17 15:31:42 +0100

    file outputs

--- lib/csv2rest/cli.rb
+++ lib/csv2rest/cli.rb
@@ -1,5 +1,6 @@
 require 'csv2rest'
 require 'pp'
+require 'fileutils'
 
 module Csv2rest
   class CLI < Thor
@@ -9,8 +10,24 @@ module Csv2rest
     end
 
     desc 'generate', 'generate JSON from CSVs'
+    method_option :output_dir,
+                  aliases: '-o',
+                  description: 'Where to output files'
     def generate csv, json
-      pp Csv2rest.generate csv, Csvlint::Schema.load_from_json(json)
+      files = Csv2rest.generate "file:"+csv, Csvlint::Schema.load_from_json(json)
+      files.each do |name, content|
+        # Index
+        name = "index" if name == ""
+        # Filename
+        filename = name + ".json"
+        FileUtils.mkdir_p options[:output_dir]
+        Dir.chdir(options[:output_dir]) do
+          # Create directories
+          FileUtils.mkdir_p File.dirname(filename)
+          # Write
+          File.write(filename, JSON.pretty_generate(content))
+        end
+      end
     end
   end
 end
