0dbdff4624f2bbce73c7916f936110b2db7d7258
James Smith <james@floppy.org.uk>
2016-06-22 11:09:28 +0100

    Add test for nested JSON objects

--- spec/nested_json_spec.rb
+++ spec/nested_json_spec.rb
@@ -7,6 +7,52 @@ describe Csv2rest do
 
     it 'generates nested JSON output' do
       g = Csv2rest.generate @schema, base_url: 'file:' + @base_url
+
+      expect(g[""]).to eq (
+        [
+          {
+           "resource"=>"schema:Country",
+           "url"=>"schema:Country"
+          }
+        ]
+      )
+
+      expect(g["schema:Country"]).to eq (
+        [
+          {"url"=>"http//example-org/country/at"},
+          {"url"=>"http//example-org/country/be"},
+          {"url"=>"http//example-org/country/bg"}
+        ]
+      )
+
+      expect(g['http//example-org/country/at']).to eq (
+        {
+          "@id" => "http://example.org/country/at",
+          "@type" => "schema:Country",
+          "schema:name" => ["Austria", "Autriche", "Osterreich"],
+          "schema:geo" => {
+            "@id" => "http://example.org/country/at#geo",
+            "schema:latitude" => 47.6965545,
+            "schema:longitude" => 13.34598005,
+            "@type" => "schema:GeoCoordinates",
+          },
+        }
+      )
+      
+      expect(g['http//example-org/country/be']).to eq (
+        {
+          "@id" => "http://example.org/country/be",
+          "@type" => "schema:Country",
+          "schema:name" => ["Belgium", "Belgique", "Belgien"],
+          "schema:geo" => {
+            "@id" => "http://example.org/country/be#geo",
+            "schema:latitude" => 50.501045,
+            "schema:longitude" =>4.47667405,
+            "@type" => "schema:GeoCoordinates",
+          }
+        }
+      )
+      
     end
   end
 end
